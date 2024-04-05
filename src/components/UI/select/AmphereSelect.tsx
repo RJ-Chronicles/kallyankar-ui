8df13cd6d73417df11f8ba54fea8ef19efce236b
Jondhalekar <2106957@cognizant.com>
2024-04-05 18:48:43 +0530

    product form select option fix

--- src/components/UI/select/AmphereSelect.tsx
+++ src/components/UI/select/AmphereSelect.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import useAppContext from "../../../hooks/useAppContext";
 import { UserFormData } from "../../../store/type";
 interface AmpProps {
@@ -12,31 +12,32 @@ const AmphereSelect: React.FC<AmpProps> = ({ setValue, value }) => {
 
   const { state } = useAppContext();
   const { amphere } = state;
-  return (
-    <div className="mb-4 md:mr-2 md:mb-0">
-      <label
-        className="block mb-2 text-sm font-bold text-gray-700"
-        htmlFor="role"
-      >
-        Amphere size
-      </label>
-      <select
-        className="w-full px-9 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-        id="role"
-        onChange={setValue}
-        value={value ?? ""}
-        name="name"
-      >
-        <option value="DEFAULT">Choose Amphere value</option>
-        {amphere?.map((data, index) => {
-          return (
+  return useMemo(
+    () => (
+      <div className="mb-4 md:mr-2 md:mb-0">
+        <label
+          className="block mb-2 text-sm font-bold text-gray-700"
+          htmlFor="role"
+        >
+          Amphere size
+        </label>
+        <select
+          className="w-full px-9 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          id="role"
+          onChange={setValue}
+          value={value ?? ""}
+          name="name"
+        >
+          <option value="DEFAULT">Choose Amphere value</option>
+          {amphere?.map((data, index) => (
             <option key={index} value={data.size}>
               {data.size}
             </option>
-          );
-        })}
-      </select>
-    </div>
+          ))}
+        </select>
+      </div>
+    ),
+    [value]
   );
 };
 
--- src/components/UI/select/BatterySelect.tsx
+++ src/components/UI/select/BatterySelect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import useAppContext from "../../../hooks/useAppContext";
 import { UserFormData } from "../../../store/type";
 interface AmpProps {
@@ -12,34 +12,33 @@ const BatterySelect: React.FC<AmpProps> = ({ setValue, value }) => {
 
   const { state } = useAppContext();
   const { batteryNames } = state;
-  useEffect(() => {
-    console.log("value change: " + value);
-  }, [value]);
-  return (
-    <div className="mb-4 md:mr-2 md:mb-0">
-      <label
-        className="block mb-2 text-sm font-bold text-gray-700"
-        htmlFor="role"
-      >
-        Battery name
-      </label>
-      <select
-        className="w-full px-9 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-        id="role"
-        onChange={setValue}
-        name="type"
-        value={value ?? ""}
-      >
-        <option value="DEFAULT">Choose battery name.</option>
-        {batteryNames?.map((data, index) => {
-          return (
+
+  return useMemo(
+    () => (
+      <div className="mb-4 md:mr-2 md:mb-0">
+        <label
+          className="block mb-2 text-sm font-bold text-gray-700"
+          htmlFor="role"
+        >
+          Battery name
+        </label>
+        <select
+          className="w-full px-9 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          id="role"
+          onChange={setValue}
+          name="type"
+          value={value ?? ""}
+        >
+          <option value="DEFAULT">Choose battery name.</option>
+          {batteryNames?.map((data, index) => (
             <option key={index} value={data.name}>
               {data.name}
             </option>
-          );
-        })}
-      </select>
-    </div>
+          ))}
+        </select>
+      </div>
+    ),
+    [value]
   );
 };
 
