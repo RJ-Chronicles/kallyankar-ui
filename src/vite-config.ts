5104cf46dff325ab69e4f82245f3292414968967
Jondhalekar <2106957@cognizant.com>
2024-04-10 13:13:33 +0530

    REMOVED vite-config file

--- src/vite-config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import svgr from "@svgr/webpack";
-
-export default defineConfig({
-  plugins: [reactRefresh(), svgr],
-});
